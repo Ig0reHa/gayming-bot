29f720ab44e382b39a42789500a175aa0e817a39
Ig0reHa <ig0rehatrader@gmail.com>
2023-01-12 00:10:21 +0100

    Port setup

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 // Heroku port setup
 
-// const http = require("http");
-// const PORT = process.env.PORT || 3000;
-// const server = http.createServer((req, res) => {});
+const http = require("http");
+const PORT = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {});
 
-// server.listen(PORT, () => {
-//   console.log(`Our app is running on port ${PORT}`);
-// });
+server.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
 
 // Telegram Bot
 
